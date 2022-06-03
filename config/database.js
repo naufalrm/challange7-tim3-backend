85df1588170375e0efa53300f5820afcbb431b8a
Naufal Rizqullah <naufalrm312@gmail.com>
2022-06-03 16:18:54 +0700

    update database config

--- config/database.js
+++ config/database.js
@@ -26,7 +26,7 @@ module.exports = {
   production: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_production`,
+    database: DB_NAME,
     host: DB_HOST,
     dialect: "postgres",
     dialectOptions:{
